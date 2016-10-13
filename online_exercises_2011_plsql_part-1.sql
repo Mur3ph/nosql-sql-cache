7be1cc8476452e4837cc0b6b34cc7a735018269a
Mur3ph <paulmurphymainstreet@gmail.com>
2016-10-13 12:13:36 +0100

    simple plsql script to update employee salary

--- online_exercises_2011_plsql_part-1.sql
+++ online_exercises_2011_plsql_part-1.sql
@@ -1,15 +1,23 @@
 -- Write a program to interchange the salaries of employee 120 and 122 ?
--- Other guy 
-Declare
-   V_salary_120   employees.salary%type;
-Begin
-  Select  salary into v_salary_120
-  From employees where  employee_id = 120;
 
-  Update employees set salary  = ( select salary from employees where employee_id = 122)
-  Where employee_id = 120;
+-- Mine 
+DECLARE
+   employeeSalary EMPLOYEES.salary%type;
+BEGIN
+  SELECT  salary INTO employeeSalary
+  FROM EMPLOYEES 
+  WHERE employee_id = 120;
 
-  Update employees set salary  =  v_salary_120  Where employee_id = 122;
+  UPDATE EMPLOYEES 
+  SET salary = ( SELECT salary 
+                 FROM EMPLOYEES 
+                 WHERE employee_id = 122)
+  WHERE employee_id = 120;
 
-  Commit;
-End;
+  UPDATE EMPLOYEES 
+  SET salary = employeeSalary 
+  WHERE employee_id = 122;
+
+  COMMIT;
+END;
+
