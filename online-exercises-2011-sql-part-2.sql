420761ea5e8cc3784a3fe5837daa1023af0dc414
Mur3ph <paulmurphymainstreet@gmail.com>
2016-10-19 10:05:34 +0100

    Display employees who joined in the month of May ?

--- online-exercises-2011-sql-part-2.sql
+++ online-exercises-2011-sql-part-2.sql
@@ -1,2 +1,6 @@
-
+-- 1). Display employees who joined in the month of May ?
+SELECT TO_CHAR(HIRE_DATE,'MONTH')  FROM EMPLOYEES;
+SELECT FIRST_NAME, LAST_NAME, JOB_ID
+FROM EMPLOYEES
+WHERE TO_CHAR(HIRE_DATE,'mon') = 'may';
 
