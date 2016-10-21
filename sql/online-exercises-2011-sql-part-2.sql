0aa31928a0efa30bed194b27906f33f0680b8542
Mur3ph <paulmurphymainstreet@gmail.com>
2016-10-21 12:26:51 +0100

    added some comments

--- sql/online-exercises-2011-sql-part-2.sql
+++ sql/online-exercises-2011-sql-part-2.sql
@@ -18,5 +18,6 @@ FROM EMPLOYEES e
 INNER JOIN JOB_HISTORY jh
 ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID;
 
+-- NOTE: LAST_DAY gets the last day of the month and people accordin to this database get paid on the first of the month, so plus one
 SELECT FIRST_NAME, LAST_NAME, HIRE_DATE, LAST_DAY(HIRE_DATE)+1 AS FIRST_SALARY
 FROM EMPLOYEES;
