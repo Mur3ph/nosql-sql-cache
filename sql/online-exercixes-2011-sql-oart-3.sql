04d94a2bb0245bf1a2b2e92077106d9a65512e67
Mur3ph <paulmurphymainstreet@gmail.com>
2016-11-04 09:32:03 +0000

    Display employee ID and the date on which he ended his previous job ?

--- sql/online-exercixes-2011-sql-oart-3.sql
+++ sql/online-exercixes-2011-sql-oart-3.sql
@@ -2,3 +2,20 @@
 SELECT EXTRACT(DAY FROM (SYSDATE - CAST(TO_DATE('1.1.' || 2011, 'DD.MM.YYYY') AS TIMESTAMP ))) AS DAYS_BETWEEN_TWO_DATES FROM DUAL;
 SELECT EXTRACT(DAY FROM SYSDATE - CAST(DATE '2011-01-01' AS TIMESTAMP)) AS DAYS_BETWEEN_TWO_DATES FROM DUAL;
 SELECT SYSDATE - to_date('01-jan-2011') AS DAYS_BETWEEN_TWO_DATES FROM DUAL;
+
+-- 2). Display how many employees joined in each month of the current year ?
+
+
+-- 3). Display manager ID and number of employees managed by the manager ?
+
+
+-- 4). Display employee ID and the date on which he ended his previous job ? 
+SELECT e.EMPLOYEE_ID, MAX(jh.END_DATE) AS END_DATE_OF_PREVIOUS_JOB
+FROM EMPLOYEES e
+INNER JOIN JOB_HISTORY jh
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+GROUP BY e.EMPLOYEE_ID;
+
+SELECT EMPLOYEE_ID, MAX(END_DATE) 
+FROM JOB_HISTORY 
+GROUP BY EMPLOYEE_ID;
