608e9ea4c407172b74f899b7dbb662fca4279735
Mur3ph <paulmurphymainstreet@gmail.com>
2016-11-17 12:28:42 +0000

    Display departments in which more than five employees have commission percentage ?

--- sql/online-exercises-2011-sql-part-4.sql
+++ sql/online-exercises-2011-sql-part-4.sql
@@ -1,1 +1,26 @@
--- 1). 
+-- 1). Display departments in which more than five employees have commission percentage ?
+SELECT department_id, commission_pct
+FROM employees;
+
+SELECT department_id
+FROM employees
+WHERE commission_pct IS NOT NULL;
+
+SELECT department_id, COUNT(employee_id)
+FROM employees 
+WHERE commission_pct IS NOT NULL
+GROUP BY department_id;
+
+SELECT department_id
+FROM employees  
+WHERE commission_pct IS NOT NULL
+GROUP BY department_id
+HAVING COUNT(employee_id) > 5;
+
+SELECT department_name
+FROM employees e INNER JOIN departments d
+ON e.department_id = d.department_id
+WHERE commission_pct IS NOT NULL
+GROUP BY department_name
+HAVING COUNT(employee_id) > 5;
+
