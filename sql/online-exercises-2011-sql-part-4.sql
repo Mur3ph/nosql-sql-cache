6a56d8274ed3ed7579097fe01e6505eb2b77a7b5
Mur3ph <paulmurphymainstreet@gmail.com>
2016-11-21 15:06:24 +0000

    Display job ID of jobs that were done by more than 3 employees for more than 100 days ?

--- sql/online-exercises-2011-sql-part-4.sql
+++ sql/online-exercises-2011-sql-part-4.sql
@@ -54,5 +54,31 @@ ON e.employee_id = jh.employee_id
 GROUP BY e.employee_id 
 HAVING COUNT(e.employee_id) > 1;
 
+-- 3). Display job ID of jobs that were done by more than 3 employees for more than 100 days ?
+SELECT job_id, employee_id, start_date, end_date
+FROM job_history;
 
+SELECT jh.job_id, jh.employee_id, e.employee_id
+FROM job_history jh, employees e;
+
+SELECT (jh.end_date - jh.start_date) AS diff
+FROM job_history jh;
+
+SELECT job_id 
+FROM job_history
+GROUP BY job_id
+HAVING COUNT(job_id) > 3;
+
+SELECT job_id 
+FROM job_history
+WHERE end_date - start_date > 100
+GROUP BY job_id
+HAVING COUNT(job_id) > 3;
+
+SELECT jh.job_id
+FROM job_history jh INNER JOIN employees e
+ON jh.employee_id = e.employee_id
+WHERE jh.end_date - jh.start_date > 100
+GROUP BY jh.job_id
+HAVING COUNT(e.job_id) > 1;
 
