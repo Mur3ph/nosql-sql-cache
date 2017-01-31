01131192f2d10b9820abf85c55ddcd23c50afa17
Mur3ph <paulmurphymainstreet@gmail.com>
2017-01-31 14:24:01 +0000

    Q41). Display department name and number of employees in the department ? - Part deux

--- sql/online-exercises-2011-sql-part-5.sql
+++ sql/online-exercises-2011-sql-part-5.sql
@@ -2,11 +2,20 @@
 DESCRIBE departments;
 SELECT * FROM departments;
 
-SELECT d.department_name, COUNT(*) 
+SELECT d.department_name, COUNT(*) AS num_of_employees
 FROM employees e NATURAL JOIN departments d 
 GROUP BY d.department_name;
 
-SELECT d.department_name, COUNT(*) 
+SELECT d.department_name, COUNT(e.employee_id) AS num_of_employees
+FROM employees e NATURAL JOIN departments d 
+GROUP BY d.department_name;
+
+SELECT d.department_name, COUNT(e.employee_id) AS num_of_employees
 FROM employees e INNER JOIN departments d 
-WHERE e.employee_id = d.manager_id
+ON e.manager_id = d.manager_id
+GROUP BY d.department_name;
+
+SELECT d.department_name, COUNT(e.employee_id) AS num_of_employees
+FROM employees e INNER JOIN departments d 
+ON e.employee_id = d.manager_id
 GROUP BY d.department_name;
