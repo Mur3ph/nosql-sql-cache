c81b7c4a81ace07141271e83aac1f490164eedd2
Mur3ph <paulmurphymainstreet@gmail.com>
2017-02-06 14:47:34 +0000

    Display job title, employee ID, number of days between ending date and starting date for all jobs in department 30 from job history ?

--- sql/online-exercises-2011-sql-part-5.sql
+++ sql/online-exercises-2011-sql-part-5.sql
@@ -19,3 +19,15 @@ SELECT d.department_name, COUNT(e.employee_id) AS num_of_employees
 FROM employees e INNER JOIN departments d 
 ON e.employee_id = d.manager_id
 GROUP BY d.department_name;
+
+-- 2). Display job title, employee ID, number of days between ending date and starting date for all jobs in department 30 from job history ?
+SELECT * FROM job_history;
+
+SELECT j.job_title, jh.employee_id, EXTRACT(DAY FROM jh.end_date - EXTRACT(DAY FROM jh.start_date)) AS num_of_days
+FROM jobs j INNER JOIN job_history jh
+ON jh.job_id = j.job_id
+WHERE department_id = 30;
+
+SELECT employee_id, job_title, end_date-start_date DAYS 
+FROM job_history NATURAL JOIN jobs 
+WHERE department_id=30;
