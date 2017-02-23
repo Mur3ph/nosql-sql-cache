94484dc7039b6524b44d6eb0bc7d81ca57c2cb80
Mur3ph <paulmurphymainstreet@gmail.com>
2017-02-23 15:13:15 +0000

    Q52. Display employee name if the employee joined before his manager ?

--- sql/online-exercises-2011-sql-part-6.sql
+++ sql/online-exercises-2011-sql-part-6.sql
@@ -13,3 +13,14 @@ SELECT d.department_name, e.first_name || ' ' || e.last_name, e.salary, (SYSDATE
 FROM departments d INNER JOIN employees e 
 ON d.manager_id = e.manager_id 
 WHERE (SYSDATE - e.hire_date) / 365 > 5;
+
+-- 2). Display employee name if the employee joined before his manager ?
+SELECT e.first_name || ' ' || e.last_name AS employee_name, e.hire_date AS employee, m.hire_date AS manager, e.manager_id, m.manager_id, m.first_name || ' ' || m.last_name AS manager_name
+FROM employees e INNER JOIN employees m
+ON e.employee_id = m.manager_id
+WHERE e.hire_date < m.hire_date;
+
+SELECT e.first_name || ' ' || e.last_name AS fullname
+FROM employees e INNER JOIN employees m
+ON e.employee_id = m.manager_id
+WHERE e.hire_date < m.hire_date;
