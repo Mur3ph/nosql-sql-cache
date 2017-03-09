d601c4598754435d1f4faf51ff2014f21323e333
Mur3ph <paulmurphymainstreet@gmail.com>
2017-03-09 12:49:35 +0000

    Q.57). Display details of departments in which the maximum salary is more than 10000 ?

--- sql/online-exercises-2011-sql-part-6.sql
+++ sql/online-exercises-2011-sql-part-6.sql
@@ -97,7 +97,26 @@ WHERE  CITY = 'Seattle'
 GROUP BY e.hire_date
 HAVING COUNT(*) > 5;
 
+-- 7). Display details of departments in which the maximum salary is more than 10000 ?
+SELECT d.department_name, d.manager_id, d.location_id
+FROM departments d INNER JOIN employees e
+ON d.department_id = e.department_id
+INNER JOIN jobs j
+ON e.job_id = j.job_id
+WHERE j.max_salary > 10000;
 
+SELECT d.department_id, d.department_name, d.manager_id, d.location_id
+FROM departments d INNER JOIN employees e
+ON d.department_id = e.department_id
+GROUP BY d.department_id, d.department_name, d.manager_id, d.location_id
+HAVING MAX(e.salary) > 10000
+ORDER BY d.department_id;
+
+SELECT * FROM departments d WHERE d.department_id IN 
+( SELECT e.department_id FROM employees e 
+  GROUP BY e.department_id 
+  HAVING MAX(e.salary) > 10000)
+ORDER BY d.department_id;
 
 
 
