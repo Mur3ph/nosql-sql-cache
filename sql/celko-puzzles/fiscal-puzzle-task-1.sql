d2f0b58a6aafc9f85d4dd4fb974a0e7aeca227e4
Mur3ph <paulmurphymainstreet@gmail.com>
2017-07-05 15:50:34 +0100

    Added many constraints on dates

--- sql/celko-puzzles/fiscal-puzzle-task-1.sql
+++ sql/celko-puzzles/fiscal-puzzle-task-1.sql
@@ -4,6 +4,12 @@ CREATE TABLE FiscalYearTable1
   fiscal_year     INTEGER NOT NULL,
   start_date      DATE    NOT NULL,
   end_date        DATE    NOT NULL,
-  CONSTRAINT check_dates CHECK (start_date < end_date),
-  CONSTRAINT pk_fiscal_year PRIMARY KEY (fiscal_year_id)
+  CONSTRAINT pk_fiscal_year                   PRIMARY KEY (fiscal_year_id),
+  CONSTRAINT check_dates                      CHECK (start_date < end_date),
+  CONSTRAINT check_is_start_date_year_valid   CHECK ( (EXTRACT(YEAR FROM start_date)) = fiscal_year - 1),
+  CONSTRAINT check_is_start_date_month_valid  CHECK ( (EXTRACT(MONTH FROM start_date)) = 10),
+  CONSTRAINT check_is_start_date_day_valid    CHECK ( (EXTRACT(DAY FROM start_date)) = 01),
+  CONSTRAINT check_is_end_date_year_valid     CHECK ( (EXTRACT(YEAR FROM end_date)) = fiscal_year),
+  CONSTRAINT check_is_end_date_month_valid    CHECK ( (EXTRACT(MONTH FROM end_date)) = 09),
+  CONSTRAINT check_is_end_date_day_valid      CHECK ( (EXTRACT(DAY FROM end_date)) = 30)
 );
