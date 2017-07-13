2d8711385e40a1bcb0e08d779022fe6446ae911d
Mur3ph <paulmurphymainstreet@gmail.com>
2017-07-13 16:01:04 +0100

    Insert into all abseteeism tables

--- sql/celko-puzzles/absentee-puzzle-task.sql
+++ sql/celko-puzzles/absentee-puzzle-task.sql
@@ -2,6 +2,10 @@ DROP TABLE Absenteeism;
 DROP TABLE ExcuseList;
 DROP TABLE Personel;
 
+TRUNCATE TABLE Absenteeism;
+TRUNCATE TABLE ExcuseList;
+TRUNCATE TABLE Personel;
+
 -- Task 2: Missing days at work
 
 CREATE TABLE Personel
@@ -32,3 +36,27 @@ CREATE TABLE Absenteeism
   CONSTRAINT fk_excuse_list_absent    FOREIGN KEY (reason_code) REFERENCES ExcuseList,
   CONSTRAINT ck_severity_points       CHECK (severity_points BETWEEN 0 AND 4)
 );
+
+-- INSERTS INTO Personnel table
+INSERT INTO Personel (employee_id, first_name, last_name)
+  VALUES (1, 'Minnie', 'Wang');
+INSERT INTO Personel (employee_id, first_name, last_name)
+  VALUES (2, 'Joey', 'TheLips');
+INSERT INTO Personel (employee_id, first_name, last_name)
+  VALUES (3, 'Joe', 'Bloggs');
+
+-- INSERTS INTO Excuse List table
+INSERT INTO ExcuseList (reason_code, employee_id, excuse)
+  VALUES ('Hit by beer truck.!', 1, 'Hit by beer truck.!');
+INSERT INTO ExcuseList (reason_code, employee_id, excuse)
+  VALUES ('Bad hair day!', 2, 'Bad hair day!');
+INSERT INTO ExcuseList (reason_code, employee_id, excuse)
+  VALUES ('Dog ate my homework!', 3, 'Dog ate my homework!');
+
+-- INSERTS INTO Absenteeism table
+INSERT INTO Absenteeism (employee_id, absent_date, reason_code, severity_points)
+  VALUES (1, TO_DATE('2016/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'Hit by beer truck.!', 1);
+INSERT INTO Absenteeism (employee_id, absent_date, reason_code, severity_points)
+  VALUES (2, TO_DATE('2015/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'Bad hair day!', 2);
+INSERT INTO Absenteeism (employee_id, absent_date, reason_code, severity_points)
+  VALUES (3, TO_DATE('2017/05/03 21:02:44', 'yyyy/mm/dd hh24:mi:ss'), 'Dog ate my homework!', 3);
